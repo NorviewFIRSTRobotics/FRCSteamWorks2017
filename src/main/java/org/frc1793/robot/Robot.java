b6c976610d5ba62a5f70b343e8636b8584299292
Tyler Marshall <primetoxinzz@gmail.com>
2017-01-08 15:56:31 -0500

    Add Basic Timed Drive with test. Add missing strongback library

--- src/main/java/org/frc1793/robot/Robot.java
+++ src/main/java/org/frc1793/robot/Robot.java
@@ -1,14 +1,14 @@
 package org.frc1793.robot;
 
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import org.frc1793.robot.commands.TimedDriveCommand;
 import org.strongback.Strongback;
-import org.strongback.command.Command;
 import org.strongback.components.Motor;
 import org.strongback.components.ui.ContinuousRange;
 import org.strongback.components.ui.FlightStick;
 import org.strongback.drive.TankDrive;
 import org.strongback.hardware.Hardware;
-
-import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
     private TankDrive drive;
@@ -16,36 +16,38 @@ public class Robot extends IterativeRobot {
     private ContinuousRange turnSpeed;
     @Override
     public void robotInit() {
-        Strongback.configure().recordNoEvents().recordNoData();
+        Strongback.configure().recordNoEvents();
 
-        Motor left = Motor.compose(Hardware.Motors.talon(1), Hardware.Motors.talon(2));
-        Motor right = Motor.compose(Hardware.Motors.talon(3), Hardware.Motors.talon(4));
+        Motor left = Motor.compose(Hardware.Motors.talonSRX(1), Hardware.Motors.talonSRX(2));
+        Motor right = Motor.compose(Hardware.Motors.talonSRX(3), Hardware.Motors.talonSRX(4));
 
         drive = new TankDrive(left, right);
 
+        // Set up the human input controls for teleoperated mode. We want to use the Logitech Attack 3D's throttle as a
+        // "sensitivity" input to scale the drive speed and throttle, so we'll map it from it's native [-1,1] to a simple scale
+        // factor of [0,1] ...
         FlightStick joystick = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
-        driveSpeed = joystick.getPitch();
-        turnSpeed = joystick.getRoll().invert();
+        ContinuousRange sensitivity = joystick.getThrottle().map(t -> (t + 1.0) / 2.0);
+        driveSpeed = joystick.getPitch().scale(sensitivity::read);
+        turnSpeed = joystick.getRoll().scale(sensitivity::read).invert();
+
+        // Set up the data recorder to capture the left & right motor speeds (since both motors on the same side should
+        // be at the same speed, we can just use the composed motors for each) and the sensitivity. We have to do this
+        // before we start Strongback...
+        Strongback.dataRecorder()
+                .register("Left motors", left)
+                .register("Right motors", right)
+                .register("Sensitivity", sensitivity.scaleAsInt(1000));
     }
     @Override
     public void autonomousInit() {
         Strongback.start();
-        Strongback.submit(new Command(5.0) {
-            @Override
-            public boolean execute() {
-                drive.arcade(0.5,0);
-                return false;
-            }
-
-            @Override
-            public void end() {
-                drive.stop();
-            }
-        });
-
+        Strongback.submit(new TimedDriveCommand(drive,0.5,0.5,false,5.0));
     }
     @Override
     public void teleopInit() {
+        // Kill anything running if it is ...
+        Strongback.disable();
         // Start Strongback functions ...
         Strongback.start();
     }
@@ -61,4 +63,9 @@ public class Robot extends IterativeRobot {
         Strongback.disable();
     }
 
+
+    @Override
+    public void testInit() {
+        LiveWindow.run();
+    }
 }
---
+++ src/main/java/org/frc1793/robot/commands/FireHighGoalCommand.java
@@ -0,0 +1,16 @@
+package org.frc1793.robot.commands;
+
+import org.strongback.command.Command;
+
+/**
+ * Purpose:
+ *
+ * @author Tyler Marshall
+ * @version 1/8/17
+ */
+public class FireHighGoalCommand extends Command {
+    @Override
+    public boolean execute() {
+        return false;
+    }
+}
