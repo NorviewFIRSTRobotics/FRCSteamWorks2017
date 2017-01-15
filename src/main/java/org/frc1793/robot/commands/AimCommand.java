87315f6c1bd0ffd6fac406fa8174095433455c6b
Tyler Marshall <primetoxinzz@gmail.com>
2017-01-15 13:30:15 -0500

    Test for AimCommand

--- src/main/java/org/frc1793/robot/commands/AimCommand.java
+++ src/main/java/org/frc1793/robot/commands/AimCommand.java
@@ -14,24 +14,26 @@ public class AimCommand extends Command {
 
     private double angle;
     private Motor motor;
+    private double speed;
     private AngleSensor angleSensor;
 
 
     /**
-     *
-     * @param angle number in degrees to set the launcher to aim at.
+     *  @param angle number in degrees to set the launcher to aim at.
      * @param motor supplied motor controller to change to the necessary angle.
+     * @param speed speed for the motor.
      * @param angleSensor sensor to supply the current position of motor
      */
-    public AimCommand(double angle, Motor motor, AngleSensor angleSensor) {
+    public AimCommand(double angle, Motor motor, double speed, AngleSensor angleSensor) {
         this.angle = angle;
         this.motor = motor;
+        this.speed = speed;
         this.angleSensor = angleSensor;
     }
     @Override
     public boolean execute() {
-        double dir = angleSensor.computeAngleChangeTo(angle,1);
-        motor.setSpeed(dir > 0? 1 : -1);
+        double dir = angleSensor.computeAngleChangeTo(angle,0.1);
+        motor.setSpeed(dir == 0? 0: dir > 0? speed: -speed);
         return false;
     }
 
--- src/test/java/org/frc1793/robot/AimCommandTest.java
+++ src/test/java/org/frc1793/robot/AimCommandTest.java
@@ -1,14 +1,65 @@
 package org.frc1793.robot;
 
-import org.strongback.command.Command;
+import org.fest.assertions.Assertions;
+import org.fest.assertions.Delta;
+import org.frc1793.robot.commands.AimCommand;
+import org.junit.Before;
+import org.junit.Test;
+import org.strongback.command.CommandTester;
+import org.strongback.components.Motor;
+import org.strongback.mock.Mock;
+import org.strongback.mock.MockAngleSensor;
 
 /**
  * Created by melvin on 1/15/2017.
  */
-public class AimCommandTest extends Command {
+public class AimCommandTest  {
+    private final Delta TOLERANCE = Delta.delta(0.001);
+    private final long START_TIME_MS = 1000;
 
-    @Override
-    public boolean execute() {
-        return false;
+    private Motor motor;
+    private MockAngleSensor angleSensor;
+    private CommandTester tester;
+
+    @Before
+    public void beforeEach() {
+        motor = Mock.stoppedMotor();
+        angleSensor = Mock.angleSensor();
+    }
+
+    @Test
+    public void shouldStopAfterAngleReached() {
+        final double SPEED = 1;
+        tester = new CommandTester(new AimCommand(AimCommand.LOW_GOAL,motor, SPEED, angleSensor));
+        Assertions.assertThat(motor.getSpeed()).isEqualTo(0,TOLERANCE);
+
+        // Start the command with the given artificial start time ...
+        tester.step(START_TIME_MS);
+
+        angleSensor.setAngle(0);
+        tester.step(START_TIME_MS+1);
+        Assertions.assertThat(motor.getSpeed()).isEqualTo(1,TOLERANCE);
+
+        angleSensor.setAngle(120);
+        tester.step(START_TIME_MS+2);
+        Assertions.assertThat(motor.getSpeed()).isEqualTo(-1,TOLERANCE);
+
+        angleSensor.setAngle(90);
+        tester.step(START_TIME_MS+3);
+        Assertions.assertThat(motor.getSpeed()).isEqualTo(0,TOLERANCE);
+    }
+
+    @Test
+    public void shouldStopWhenCancelled() {
+        final double SPEED = 1;
+        tester = new CommandTester(new AimCommand(AimCommand.LOW_GOAL,motor, SPEED, angleSensor));
+        angleSensor.setAngle(0);
+        tester.step(START_TIME_MS);
+        Assertions.assertThat(motor.getSpeed()).isEqualTo(1,TOLERANCE);
+
+        tester.cancel();
+        tester.step(START_TIME_MS);
+
+        Assertions.assertThat(motor.getSpeed()).isEqualTo(0,TOLERANCE);
     }
 }
