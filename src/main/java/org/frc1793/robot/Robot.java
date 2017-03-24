ae02d7fffbd6dad46a946c0626472c664796f7a4
tyler <primetoxinzz@gmail.com>
2017-03-24 08:18:07 -0400

    Put back right shooter

--- src/main/java/org/frc1793/robot/Robot.java
+++ src/main/java/org/frc1793/robot/Robot.java
@@ -2,6 +2,11 @@ package org.frc1793.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.frc1793.robot.commands.agitator.AgitateStartCommand;
+import org.frc1793.robot.commands.agitator.AgitateStopCommand;
+import org.frc1793.robot.commands.firing.ContinuousFireCommand;
+import org.frc1793.robot.commands.firing.DisableFireCommand;
 import org.frc1793.robot.commands.sweeper.SweeperStartCommand;
 import org.frc1793.robot.commands.sweeper.SweeperStopCommand;
 import org.frc1793.robot.components.HopperAgitators;
@@ -28,31 +33,28 @@ import org.strongback.hardware.Hardware;
 import static org.frc1793.robot.Config.isControllerDrive;
 import static org.frc1793.robot.utils.Utils.logitechDualAction;
 import static org.frc1793.robot.utils.Utils.microsoftSideWinder;
+import static org.frc1793.robot.utils.Utils.switchFromDpad;
 
 @SuppressWarnings("ALL")
 public class Robot extends IterativeRobot {
 
 
     private TankDrive drive;
-    private Shooter leftShooter, rightShooter;
+    private Shooter rightShooter;
     private HopperAgitators agitators;
     private Sweeper sweeper;
     private DriverCamera driverCamera;
     private Vision vision;
-
     private PositionCalculator position;
+    private CurrentSensor current;
+    private VoltageSensor battery;
+    private Autonomous autonomous;
 
-    private SettableRange leftShooterSpeed;
     private SettableRange rightShooterSpeed;
-
     private ContinuousRange driveSpeed;
     private ContinuousRange turnSpeed;
     private ContinuousRange sweeperSpeed;
     private ContinuousRange agitatorSpeed;
-
-    private CurrentSensor current;
-    private VoltageSensor battery;
-    private Autonomous autonomous;
 
     @Override
     public void robotInit() {
@@ -65,15 +67,14 @@ public class Robot extends IterativeRobot {
         Motor left = Motor.compose(Hardware.Motors.talon(0), Hardware.Motors.talon(1));
         Motor right = Motor.compose(Hardware.Motors.talon(2), Hardware.Motors.talon(3)).invert();
         this.drive = new TankDrive(left, right);
-
         //hallo from bee! :3
-//        this.leftShooter = new Shooter(Hardware.Controllers.talonController(0, 1, 1));
-//        this.rightShooter = new Shooter(Hardware.Controllers.talonController(1, 1, 1));
+        this.rightShooter = new Shooter(Hardware.Motors.spark(4));
+
         this.sweeper = new Sweeper(Hardware.Motors.talonSRX(0), Hardware.Motors.talonSRX(1));
 
         this.agitators = new HopperAgitators(Hardware.Motors.victor(5));
+
         this.driverCamera = new DriverCamera();
-//        this.vision = new Vision();
 
         this.autonomous = new Autonomous(drive);
 
@@ -89,7 +90,6 @@ public class Robot extends IterativeRobot {
         Config.update();
 
         autonomous.init();
-//        Strongback.submit(new TimedDriveCommand(this.drive, 0.9,-0.75, 0));
     }
 
     @Override
@@ -108,7 +108,7 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
-//        vision.run();
+        Config.update();
         drive.arcade(driveSpeed.read(), turnSpeed.read());
     }
 
@@ -128,7 +128,7 @@ public class Robot extends IterativeRobot {
         FlightStick driveStick = microsoftSideWinder(0);
         Gamepad controller = logitechDualAction(1);
         SwitchReactor reactor = Strongback.switchReactor();
-        if (isControllerDrive.getAsBoolean()) {
+        if (isControllerDrive.get()) {
             driveSpeed = controller.getLeftY().map(Utils::finesseControl);
             turnSpeed = controller.getLeftX().map(n -> Utils.finesseControl(n)).invert();
         } else {
@@ -139,37 +139,20 @@ public class Robot extends IterativeRobot {
         sweeperSpeed = controller.getRightY();
         reactor.onTriggered(controller.getA(), new SwitchToggle(new SweeperStartCommand(sweeper, sweeperSpeed), new SweeperStopCommand(sweeper))::run);
 
+        rightShooterSpeed = new SettableRange(0, 1, Config.rightShooterInitialSpeed.get());
 
+        reactor.onTriggered(controller.getX(), new SwitchToggle(new AgitateStartCommand(agitators, agitatorSpeed), new AgitateStopCommand(agitators))::run);
 
-//        reactor.onTriggeredSubmit(driveStick.getTrigger(), () -> new TimedDriveCommand(drive, Config.turnTime.getAsDouble(), 0, Config.turnSpeed.getAsDouble()));
-        //        agitatorSpeed = () -> 0.5;
+        reactor.onTriggered(controller.getButton(10), new SwitchToggle(new ContinuousFireCommand(rightShooter, rightShooterSpeed.invert()), new DisableFireCommand(rightShooter))::run);
 
-        rightShooterSpeed = new SettableRange(0, 1, Config.rightShooterInitialSpeed.getAsDouble());
-        leftShooterSpeed = new SettableRange(0, 1, Config.leftShooterInitialSpeed.getAsDouble());
-
-//        reactor.onTriggered(controller.getA(), new SwitchToggle(new SweeperStartCommand(sweeper, sweeperSpeed), new SweeperStopCommand(sweeper))::run);
-//        reactor.onTriggered(controller.getX(), new SwitchToggle(new AgitateStartCommand(agitators, agitatorSpeed), new AgitateStopCommand(agitators))::run);
-
-//        reactor.onTriggered(controller.getButton(9), new SwitchToggle(new ContinuousFireCommand(leftShooter, leftShooterSpeed), new DisableFireCommand(leftShooter))::run);
-//        reactor.onTriggered(controller.getButton(10), new SwitchToggle(new ContinuousFireCommand(rightShooter, rightShooterSpeed.invert()), new DisableFireCommand(rightShooter))::run);
-//
-//        reactor.onTriggered(switchFromDpad(controller.getDPad(0), 270), () -> {
-//            rightShooterSpeed.increment(0.01);
-//            SmartDashboard.putNumber("right shooter speed", rightShooterSpeed.read() * 100);
-//        });
-//        reactor.onTriggered(switchFromDpad(controller.getDPad(0), 90), () -> {
-//            rightShooterSpeed.decrement(00.01);
-//            SmartDashboard.putNumber("right shooter speed", rightShooterSpeed.read() * 100);
-//        });
-//
-//        reactor.onTriggered(switchFromDpad(controller.getDPad(0), 0), () -> {
-//            leftShooterSpeed.increment(0.01);
-//            SmartDashboard.putNumber("left shooter speed", leftShooterSpeed.read() * 100);
-//        });
-//        reactor.onTriggered(switchFromDpad(controller.getDPad(0), 180), () -> {
-//            leftShooterSpeed.decrement(0.01);
-//            SmartDashboard.putNumber("left shooter speed", leftShooterSpeed.read() * 100);
-//        });
+        reactor.onTriggered(switchFromDpad(controller.getDPad(0), 270), () -> {
+            rightShooterSpeed.increment(0.01);
+            SmartDashboard.putNumber("right shooter speed", rightShooterSpeed.read() * 100);
+        });
+        reactor.onTriggered(switchFromDpad(controller.getDPad(0), 90), () -> {
+            rightShooterSpeed.decrement(00.01);
+            SmartDashboard.putNumber("right shooter speed", rightShooterSpeed.read() * 100);
+        });
 
     }
 
--- src/main/java/org/frc1793/robot/components/Shooter.java
+++ src/main/java/org/frc1793/robot/components/Shooter.java
@@ -2,10 +2,12 @@ package org.frc1793.robot.components;
 
 import org.frc1793.robot.Config;
 import org.strongback.command.Requirable;
+import org.strongback.components.Motor;
 import org.strongback.components.TalonSRX;
 import org.strongback.components.ui.ContinuousRange;
 import org.strongback.control.PIDController;
 import org.strongback.control.TalonController;
+import org.strongback.hardware.Hardware;
 
 /**
  * Created by melvin on 1/19/2017.
@@ -14,24 +16,13 @@ import org.strongback.control.TalonController;
  */
 public class Shooter extends BasicMotor implements Requirable {
 
-    public Shooter(TalonSRX talon) {
-        super(talon);
-    }
-    public Shooter(TalonController talon) {
-        super(talon);
-        ((TalonController)this.motor).setFeedbackDevice(TalonSRX.FeedbackDevice.QUADRATURE_ENCODER);
+    public Shooter(Motor motor) {
+        super(motor);
     }
 
     @Override
     public void start(ContinuousRange speed) {
-        if(!(motor instanceof PIDController))
-            super.start(speed);
-        else
-            fromConfig().setSpeed(speed.read());
-    }
-
-    public TalonController fromConfig() {
-        return ((TalonController)this.motor).withGains(Config.proportional.getAsDouble(), Config.integral.getAsDouble(), Config.differential.getAsDouble());
+        super.start(speed);
     }
 
 }
